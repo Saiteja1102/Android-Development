ba2aa227eb308e93e01746266f5e4d1e3c792540
Makthala Sai Teja Goud <mstg1102@gmail.com>
2024-10-22 22:20:33 +0530

    010

---
+++ Snackbar/app/src/main/java/com/example/snackbar/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.snackbar
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.*
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val snackbar = Snackbar.make(it, "This is a Snackbar", Snackbar.LENGTH_LONG)
+            snackbar.setAction("Close"){
+                Log.d("MainActivity", "Close button clicked")
+            }
+            snackbar.show()
+        }
+    }
+}
